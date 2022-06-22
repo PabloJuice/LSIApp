defa710f85c04314ac7494d194011a3738b3e0eb
Pavlo <pavlodzhus.work@gmail.com>
2022-06-22 19:07:11 +0300

    Project init

---
+++ app/src/main/java/com/pablojuice/lsiapp/ui/main/MainFragment.kt
@@ -0,0 +1,31 @@
+package com.pablojuice.lsiapp.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.pablojuice.lsiapp.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+}
